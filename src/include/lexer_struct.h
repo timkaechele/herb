587ec96f977f180507052fb989937a538b5099cb
Marco Roth <marco.roth@intergga.ch>
2025-02-11 04:40:59 +0100

    Extract `lexer_state_T` and `token_type_T` enums

--- src/include/lexer_struct.h
+++ src/include/lexer_struct.h
@@ -3,6 +3,22 @@
 
 #include <stdlib.h>
 
+typedef enum {
+  STATE_NONE,
+  STATE_SINGLE_QUOTE,
+  STATE_DOUBLE_QUOTE,
+  STATE_START_TAG_START,
+  STATE_END_TAG_START,
+  STATE_END_TAG_END,
+  STATE_TAG_ATTRIBUTES,
+  STATE_ATTRIBUTE_START,
+  STATE_ATTRIBUTE_VALUE_START,
+  STATE_ATTRIBUTE_VALUE,
+  STATE_ATTRIBUTE_VALUE_END,
+  STATE_ELEMENT_CHILDREN,
+  STATE_ERB_OPEN,
+} lexer_state_T;
+
 typedef struct LEXER_STRUCT {
   char* source;
   size_t source_length;
@@ -10,21 +26,7 @@ typedef struct LEXER_STRUCT {
   unsigned int current_position;
   unsigned int current_line;
   unsigned int current_column;
-  enum {
-    STATE_NONE,
-    STATE_SINGLE_QUOTE,
-    STATE_DOUBLE_QUOTE,
-    STATE_START_TAG_START,
-    STATE_END_TAG_START,
-    STATE_END_TAG_END,
-    STATE_TAG_ATTRIBUTES,
-    STATE_ATTRIBUTE_START,
-    STATE_ATTRIBUTE_VALUE_START,
-    STATE_ATTRIBUTE_VALUE,
-    STATE_ATTRIBUTE_VALUE_END,
-    STATE_ELEMENT_CHILDREN,
-    STATE_ERB_OPEN,
-  } state;
+  lexer_state_T state;
 } lexer_T;
 
 #endif
