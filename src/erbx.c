ae99279c41fb7989f68f6eab9c80cb245639b3b7
Marco Roth <marco.roth@intergga.ch>
2025-02-16 05:23:30 +0100

    Add `is_lexer_stuck` to avoid infinite loops when lexing

--- src/erbx.c
+++ src/erbx.c
@@ -9,14 +9,37 @@
 
 #include <stdlib.h>
 
+bool is_lexer_stuck(lexer_T* lexer, size_t* last_position, size_t* stall_counter, size_t limit) {
+  if (*last_position == lexer->current_position) {
+    (*stall_counter)++;
+    return *stall_counter > limit;
+  }
+
+  *stall_counter = 0;
+  *last_position = lexer->current_position;
+
+  return false;
+}
+
 array_T* erbx_lex(char* source) {
   lexer_T* lexer = lexer_init(source);
-  token_T* token = 0;
+  token_T* token = NULL;
 
   array_T* tokens = array_init(1);
 
+  size_t last_position = lexer->current_position;
+  size_t stall_counter = 0;
+  const size_t STALL_COUNTER_LIMIT = 5;
+
   while ((token = lexer_next_token(lexer))->type != TOKEN_EOF) {
     array_append(tokens, token);
+
+    if (is_lexer_stuck(lexer, &last_position, &stall_counter, STALL_COUNTER_LIMIT)) {
+      char lex_error[64];
+      snprintf(lex_error, sizeof(lex_error), "Lexer stuck: no progress after %zu iterations.", STALL_COUNTER_LIMIT);
+      array_append(tokens, lexer_error(lexer, lex_error));
+      return tokens;
+    }
   }
 
   array_append(tokens, token);
