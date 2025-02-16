6d3ebd0bf3c6b5b8ee41c903b7d3165b9580e2d4
Marco Roth <marco.roth@intergga.ch>
2025-02-16 07:13:46 +0100

    Remove `ERBX.lex_file` and `ERBX.lex_to_buffer` for now

--- lib/erbx.rb
+++ lib/erbx.rb
@@ -10,14 +10,6 @@ require "erbx/lex_result"
 module ERBX
   VERSION = LibERBX.erbx_version.read_string
 
-  def self.lex_to_buffer(source)
-    LibERBX::Buffer.with do |output|
-      LibERBX.erbx_lex_to_buffer(source, output.pointer)
-
-      output.read
-    end
-  end
-
   def self.lex(source)
     LexResult.new(
       LibERBX.erbx_lex(source)
@@ -38,9 +30,5 @@ module ERBX
 
       output.read
     end
-  end
-
-  def self.lex_file(path)
-    lex(File.read(path))
   end
 end
