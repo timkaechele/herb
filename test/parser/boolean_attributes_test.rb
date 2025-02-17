eb6387d12f0f6b6797f73e451bcd46b059f35749
Marco Roth <marco.roth@intergga.ch>
2025-02-17 10:16:18 +0100

    Add `Parser::BooleanAttributesTest`

---
+++ test/parser/boolean_attributes_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative "../test_helper"
+
+module Parser
+  class BooleanAttributesTest < Minitest::Spec
+    include SnapshotUtils
+
+    test "boolean attribute" do
+      assert_parsed_snapshot(%(<input required />))
+    end
+
+    test "boolean attribute without whitespace" do
+      assert_parsed_snapshot(%(<input required/>))
+    end
+
+    test "boolean attribute without whitespace and without self-closing tag" do
+      assert_parsed_snapshot(%(<input required>))
+    end
+
+    test "boolean attribute followed by regular attribute" do
+      assert_parsed_snapshot(%(<input required id="input"/>))
+    end
+
+    test "boolean attribute after regular attribute" do
+      assert_parsed_snapshot(%(<input id="input" required />))
+    end
+
+    test "boolean attribute surounded by regular attributes" do
+      assert_parsed_snapshot(%(<input class="classes" required id="ids"/>))
+    end
+  end
+end
