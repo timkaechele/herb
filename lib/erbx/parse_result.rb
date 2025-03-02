3d1e749eaed7349cbcd76ca528ee44127e922143
Marco Roth <marco.roth@intergga.ch>
2025-03-02 23:15:06 +0100

    Add `parse:project` rake task

--- lib/erbx/parse_result.rb
+++ lib/erbx/parse_result.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "json"
+
 module ERBX
   class ParseResult < Result
     attr_reader :value
@@ -7,6 +9,18 @@ module ERBX
     def initialize(value, source, warnings, errors)
       @value = value
       super(source, warnings, errors)
+    end
+
+    def failed?
+      errors.any? || value.errors.any? # TODO: this should probably be recursive
+    end
+
+    def success?
+      !failed?
+    end
+
+    def pretty_errors
+      JSON.pretty_generate(errors + value.errors)
     end
   end
 end
