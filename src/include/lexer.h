12c039175ae4a62821e1ba51a61a6ac7497f805f
Tim Kächele <mail@timkaechele.me>
2025-10-15 21:54:38 +0200

    Add arena allocator to lexer

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -1,10 +1,11 @@
 #ifndef HERB_LEXER_H
 #define HERB_LEXER_H
 
+#include "hb_arena.h"
 #include "lexer_struct.h"
 #include "token_struct.h"
 
-void lexer_init(lexer_T* lexer, const char* source);
+void lexer_init(lexer_T* lexer, hb_arena_T* allocator, const char* source);
 token_T* lexer_next_token(lexer_T* lexer);
 token_T* lexer_error(lexer_T* lexer, const char* message);
 
