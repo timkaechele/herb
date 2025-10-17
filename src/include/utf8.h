8062a1863b52a174fb611fe5a8dee5877d37a583
Tim Kächele <mail@timkaechele.me>
2025-10-17 19:04:21 +0200

    Make utf_8_sequence_length use hb_string

--- src/include/utf8.h
+++ src/include/utf8.h
@@ -1,11 +1,12 @@
 #ifndef HERB_UTF8_H
 #define HERB_UTF8_H
 
+#include "util/hb_string.h"
 #include <stdbool.h>
 #include <stdlib.h>
 
 int utf8_char_byte_length(unsigned char first_byte);
-int utf8_sequence_length(const char* str, size_t position, size_t max_length);
+uint32_t utf8_sequence_length(hb_string_T value);
 bool utf8_is_valid_continuation_byte(unsigned char byte);
 
 #endif
