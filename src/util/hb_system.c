a14c4b4c032f420bf362a28805b2a7a438adc1fe
Tim Kächele <mail@timkaechele.me>
2025-10-29 21:00:49 +0100

    Use malloc by default

--- src/util/hb_system.c
+++ src/util/hb_system.c
@@ -4,27 +4,27 @@
 #define _GNU_SOURCE
 #endif
 
-#ifdef HB_USE_MALLOC
-#include <stdlib.h>
-#else
+#ifdef HB_USE_MMAP
 #include <sys/mman.h>
+#else
+#include <stdlib.h>
 #endif
 
 void* hb_system_allocate_memory(size_t size) {
-#ifdef HB_USE_MALLOC
-  return malloc(size);
-#else
+#ifdef HB_USE_MMAP
   void* memory = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (memory == MAP_FAILED) { return NULL; }
 
   return memory;
+#else
+  return malloc(size);
 #endif
 }
 
 void hb_system_free_memory(void* ptr, size_t size) {
-#ifdef HB_USE_MALLOC
-  free(ptr);
-#else
+#ifdef HB_USE_MMAP
   munmap(ptr, size);
+#else
+  free(ptr);
 #endif
 }
