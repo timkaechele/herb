f7c2926c7bdcb129af32ec9925b32c642b849cc0
Tim Kächele <3810945+timkaechele@users.noreply.github.com>
2025-10-30 23:21:51 +0100

    C: Use `hb_string_T` in `element_source_to_string` function (#752)
    This PR starts using the `hb_string_T` for strings when returning the
    element source string.

--- src/element_source.c
+++ src/element_source.c
@@ -1,11 +1,12 @@
 #include "include/element_source.h"
+#include "include/util/hb_string.h"
 
-const char* element_source_to_string(element_source_t source) {
+hb_string_T element_source_to_string(element_source_t source) {
   switch (source) {
-    case ELEMENT_SOURCE_HTML: return "HTML";
-    case ELEMENT_SOURCE_ACTIONVIEW: return "ActionView";
-    case ELEMENT_SOURCE_HAML: return "Haml";
-    case ELEMENT_SOURCE_SLIM: return "Slim";
-    default: return "Unknown";
+    case ELEMENT_SOURCE_HTML: return hb_string("HTML");
+    case ELEMENT_SOURCE_ACTIONVIEW: return hb_string("ActionView");
+    case ELEMENT_SOURCE_HAML: return hb_string("Haml");
+    case ELEMENT_SOURCE_SLIM: return hb_string("Slim");
+    default: return hb_string("Unknown");
   }
 }
--- src/include/element_source.h
+++ src/include/element_source.h
@@ -1,6 +1,8 @@
 #ifndef HERB_ELEMENT_SOURCE_H
 #define HERB_ELEMENT_SOURCE_H
 
+#include "util/hb_string.h"
+
 typedef enum {
   ELEMENT_SOURCE_HTML,
   ELEMENT_SOURCE_ACTIONVIEW,
@@ -8,6 +10,6 @@ typedef enum {
   ELEMENT_SOURCE_SLIM
 } element_source_t;
 
-const char* element_source_to_string(element_source_t source);
+hb_string_T element_source_to_string(element_source_t source);
 
 #endif
