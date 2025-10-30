93d10abf9a48032dad32270950663c562b70cc7e
Tim Kächele <mail@timkaechele.me>
2025-10-30 21:14:04 +0100

    Return uint32_t in utf8_char_byte_length

--- src/utf8.c
+++ src/utf8.c
@@ -1,12 +1,13 @@
 #include "include/utf8.h"
 #include "include/util/hb_string.h"
+#include <stdint.h>
 
 // UTF-8 byte patterns:
 //   0xxxxxxx = 1 byte (ASCII)
 //   110xxxxx = 2 bytes
 //   1110xxxx = 3 bytes
 //   11110xxx = 4 bytes
-int utf8_char_byte_length(unsigned char first_byte) {
+uint32_t utf8_char_byte_length(unsigned char first_byte) {
   if ((first_byte & 0x80) == 0) {
     return 1;
   } else if ((first_byte & 0xE0) == 0xC0) {
@@ -28,13 +29,13 @@ bool utf8_is_valid_continuation_byte(unsigned char byte) {
 uint32_t utf8_sequence_length(hb_string_T value) {
   if (hb_string_is_empty(value)) { return 0; }
 
-  int expected_length = utf8_char_byte_length(value.data[0]);
+  uint32_t expected_length = utf8_char_byte_length(value.data[0]);
   if (value.length - expected_length < expected_length) {
     return 1; // Not enough bytes, treat as single byte
   }
 
   if (expected_length > 1) {
-    for (int i = 1; i < expected_length; i++) {
+    for (uint32_t i = 1; i < expected_length; i++) {
       if (!utf8_is_valid_continuation_byte((unsigned char) value.data[i])) {
         return 1; // Invalid continuation byte, treat first byte as single byte
       }
